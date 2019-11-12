2aab3890dd50c99520cf768bd53644ebedf550fe
pal_user <kerules.fareg@jpmchase.com>
2019-11-12 16:10:01 -0700

    Saving Env Config

--- src/test/java/test/pivotal/pal/tracker/WelcomeControllerTest.java
+++ src/test/java/test/pivotal/pal/tracker/WelcomeControllerTest.java
@@ -2,15 +2,18 @@ package test.pivotal.pal.tracker;
 
 import io.pivotal.pal.tracker.WelcomeController;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Value;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class WelcomeControllerTest {
 
     @Test
-    public void itSaysHello() throws Exception {
+      public void itSaysHello() throws Exception {
         WelcomeController controller = new WelcomeController("A welcome message");
 
         assertThat(controller.sayHello()).isEqualTo("A welcome message");
     }
 }
+
+
